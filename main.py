000b563b7d192cbfa85f03c7fb75e492918b2512
David Niebert <david.niebert@googlemail.com>
2025-01-05 20:54:18 +0100

    Save kingdoms and players

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import random
-from numpy.distutils.extension import Extension
+import os
 
 path_pages = './src/Pages.xlsx'
 path_cards = './src/Cards.csv'
+path_kingdoms = './src/Kingdoms.csv'
 columns = ['Extension', 'Name']
 
 # Generate List of card names with Pages.xlsx
@@ -35,6 +35,7 @@ current_expansions = ['baseset', 'baseset2', 'intrigue', 'intrigue2', 'seaside',
 
 fixed_cards = []
 
+
 def generate_kingdom(expansion, fixed_cards):
     generate_number = 10 - len(fixed_cards)
     cards = pd.read_csv(path_cards)
@@ -42,18 +43,89 @@ def generate_kingdom(expansion, fixed_cards):
     random_cards = cards.sample(n=generate_number, random_state=42)['Name'].tolist()
 
     kingdom = fixed_cards + random_cards
-    print(kingdom)
     return kingdom
 
+players = ["David", "Julian", "Jojo"]
 
 
+def start_game():
+    pass
 
 
-#Promos!
+def new_round():
+    """
+    This method takes care of initiating a new round. It lets you generate multiple kingdoms and choose one,
+    then it asks for the names of the players and stores the value in a csv.
+    """
+    global players
+    choice = input(f"Starting a new round of Dominion... \nThe current selected players are: {players}\nPress Enter to start a new "
+          "round or enter 'c' to change the players.")
+
+    if choice == "c":
+        players = []
+        while True:
+            player_name = input("Enter a player name or press enter when all players are named.")
+            if player_name == "":
+                break
+            else:
+                players.append(player_name)
+        if len(players) == 0:
+            raise ValueError("There are no selected players.")
+        if len(players) > 6:
+            raise ValueError("There are too many players.")
+        new_round()
+
+    if choice != "":
+        print(f"Unknown command {choice}")
+        new_round()
+
+    players.sort()
+    kingdom_cards = []
+
+    while True:
+        print(f"Generating Kingdoms with expansions:\n{current_expansions}\nand fixed cards:\n{fixed_cards}")
+        kingdom_cards = generate_kingdom(current_expansions, fixed_cards)
+
+        choice = input(f"Generated kingdom:\n{kingdom_cards}\n press enter to play the kingdom or 'r' to generate a different "
+                       f"kingdom.")
+        if choice == "":
+            break
+
+        if choice != "r":
+            raise ValueError(f"Unknown command {choice}")
+
+    write_new_kingdom(players, kingdom_cards)
+
+
+def write_new_kingdom(players, kingdom_cards):
+    kingdom_columns = ["Player1", "Player2", "Player3", "Player4", "Player5", "Player6",
+                       "Card1", "Card2", "Card3", "Card4", "Card5", "Card6", "Card7", "Card8", "Card9", "Card10",
+                       "1. Place", "2. Place", "3. Place", "4. Place", "5. Place", "6. Place",
+                       "Player1VP", "Player2VP", "Player3VP", "Player4VP", "Player5VP", "Player6VP"]
+
+    if not os.path.exists(path_kingdoms):
+        df = pd.DataFrame(columns=kingdom_columns)
+        df.to_csv(path_kingdoms, index=False)
+
+    cur_columns = kingdom_columns[:len(players)] + kingdom_columns[6:16]
+    print(cur_columns)
+    cur_row = players + kingdom_cards
+    print(cur_row)
+    new_kingdom_line = pd.DataFrame([cur_row], columns=cur_columns)
+    print(f"New Kingdom Line: {new_kingdom_line}")
+    kingdoms_csv = pd.read_csv(path_kingdoms)
+    kingdoms_csv = pd.concat([kingdoms_csv, new_kingdom_line], ignore_index=True)
+    kingdoms_csv.to_csv(path_kingdoms, index=False, header=kingdom_columns)
+
+
+def add_scores():
+    pass
+
+#TODO Promos!
 
 def main():
     create_card_storage()
-    generate_kingdom(current_expansions, [])
+    new_round()
 
 if __name__ == "__main__":
     main()
